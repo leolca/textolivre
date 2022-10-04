380db2f5ee055e7c1b11d4653d80e3c212d0efce
leolca <leolca@gmail.com>
2022-10-04 16:42:16 -0300

    updates to overleaf2github.sh

--- maintenance/overleaf2github.sh
+++ maintenance/overleaf2github.sh
@@ -4,6 +4,8 @@ ALWAYSOW=false
 OVERWRITE=false
 ALWAYSAG=false
 ADDGITHUB=false
+ALWAYSCL=false
+CLEAN=false
 CURRENTFOLDER=$(pwd)
 
 for (( i=0; i<${#IDs[@]}; i++ ));
@@ -34,7 +36,7 @@ do
 	  mkdir -p $DEST_FOLDER
       fi
 
-      if [ $OVERWRITE ]
+      if [ $OVERWRITE = true ]
       then
 	  TMPFOLDER=$(mktemp -d)
 	  trap '{ rm -rf "$TMPFOLDER"; }' INT TERM
@@ -46,14 +48,16 @@ do
 	  if [ $? -eq 0 ]
 	  then
 	      cp $TMPFOLDER/*.{pdf,png,jpg,jpeg,eps,tex,bib} $DEST_FOLDER 2>/dev/null
-	      rm -rf $TMPFOLDER/article.pdf $TMPFOLDER/internationalization.tex $TMPFOLDER/listingconfig.tex $TMPFOLDER/logo.pdf
-	      cd $TMPFOLDER
+	      rm -rf "$TMPFOLDER"
+	      rm -rf $DEST_FOLDER/article.pdf $DEST_FOLDER/internationalization.tex $DEST_FOLDER/listingconfig.tex $DEST_FOLDER/logo.pdf
+	      cd $DEST_FOLDER
 	      ln -s ../../../../template/internationalization.tex; 
 	      ln -s ../../../../template/listingconfig.tex;
 	      ln -s ../../../../template/logo.pdf;
 	      ln -s ../../../../template/textolivre.cls;
 	      ln -s ../../../../template/textolivre.cfg;
 	      xelatex article.tex; biber article; xelater article.tex; xelatex article.tex;
+	      cd $CURRENTFOLDER 
 	  fi
           if [ $ALWAYSAG = false ]; then
 	      while true; do
@@ -66,7 +70,26 @@ do
 		  esac
 	      done
 	  else
-	      OVERWRITE=true
+	      ADDGITHUB=true
+	  fi
+	  if [ $ADDGITHUB = true ]; then
+	      git add $DEST_FOLDER/*
+	  fi
+	  if [ $ALWAYSCL = false ]; then
+	      while true; do
+		  read -u 2 -p "Clen (y/n/a)?" yn
+		  case $yn in
+		      [Yy]* ) CLEAN=true; break;;
+		      [Nn]* ) CLEAN=false; exit;;
+		      [Aa]* ) ALWAYSCL=true; CLEAN=true; break;;
+		      * ) echo "Please answer yes or no.";;
+		  esac
+	      done
+	  else
+	      CLEAN=true
+	  fi
+	  if [ $CLEAN = true]; then
+	      git clean -dxf $DEST_FOLDER
 	  fi
       fi
   fi
